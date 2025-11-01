35383b3ae013a973f5d8ff90a8ba85ef13c517e0
spawnfather <thespawnfather@gmail.com>
2025-11-01 14:46:07 +0000

    Update [guildId].js

--- functions/server/[guildId].js
+++ functions/server/[guildId].js
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://unpkg.com/@supabase/supabase-js@2.46.1/dist/esm/index.js";
 
 export async function onRequestGet({ params, env }) {
   const { guildId } = params
