5e87df74bb5c798ed2303c1be60e6c13ac5d22af
spawnfather <thespawnfather@gmail.com>
2025-11-02 21:12:02 +0000

    Update [amount].js

--- functions/leaderboard/top/[amount].js
+++ functions/leaderboard/top/[amount].js
@@ -12,7 +12,7 @@ export async function onRequest(context) {
   const SUPABASE_URL = 'https://gzrsknywsqpfimeecydn.supabase.co';
   const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6cnNrbnl3c3FwZmltZWVjeWRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNzc3MDgsImV4cCI6MjA3NjY1MzcwOH0.hjBoZqa-BC41cnbknzwkM36mER2I-3gsk-hUp7CVaWA';
 
-  const response = await fetch(`${SUPABASE_URL}/rest/v1/leaderboard?select=*&order=member_count.desc&limit=${limit}`, {
+  const response = await fetch(`${SUPABASE_URL}/rest/v1/leaderboardmain?select=*&order=member_count.desc&limit=${limit}`, {
     headers: {
       'apikey': SUPABASE_ANON_KEY,
       'Authorization': `Bearer ${SUPABASE_ANON_KEY}`,
